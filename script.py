4ad7dc6da3c09da60a56a57e2deaeb6894b317c2
Abdullah Goher <137772368+Abdullah3245@users.noreply.github.com>
2025-02-27 00:19:28 -0500

    fix: fixed bug in scraping actual text

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ def scrape_data_point():
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
         target_element = soup.find("h3", class_="standard_link")
-        data_point = "" if target_element is None else target_element.text
+        data_point = "" if target_element is None else target_element.a.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
 
